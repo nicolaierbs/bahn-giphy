da379d6d47d33fd790cc16a2557a682f696c0faf
Nicolai Erbs <nico@erbs.eu>
2021-03-11 20:51:12 +0100

    Add text to image

--- image-generation.py
+++ image-generation.py
@@ -1,17 +1,22 @@
 import imageio
 import os
 from pygifsicle import optimize
-from PIL import Image
+from PIL import Image, ImageDraw, ImageFont
 
 im_landscape = Image.open(r'images/landscape-winter.jpg')
 im_train = Image.open(r'images/train_ice2.png')
 
 images = list()
-y=180
+y = 180
+font = ImageFont.truetype('xkcd-script.ttf', 32)
 for i in range(55):
     image = im_landscape.copy()
     x = i*20-550
     image.paste(im_train, (x, y), mask=im_train)
+
+    draw = ImageDraw.Draw(image)
+    draw.text((150, 60), 'Ready for DBRegioDataHack?', (255, 100, 100), font=font)
+
     images.append(image)
 
 output_gif = 'output/bahn-giphy.gif'
